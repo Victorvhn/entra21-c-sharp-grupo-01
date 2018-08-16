1872cf25b660fa801ece591199913e4a441a598e
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 17:28:25 -0300

    Idioma Repository

--- TrabalhoFinal/Repository/IdiomaRepository.cs
+++ TrabalhoFinal/Repository/IdiomaRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using Principal;
 using Principal.Database;
+using System.Data;
 
 namespace Principal.Repository
 {
@@ -14,8 +15,69 @@ namespace Principal.Repository
         public List<Idioma> ObterTodos()
         {
             List<Idioma> idiomas = new List<Idioma>();
-            SqlCommand command = new BancoDados().ObterConexao();
+            SqlCommand command = new Conexao().ObterConexao();
+
+            command.CommandText = "SELECT id, nome, id_guia FROM idiomas";
+            DataTable table = new DataTable();
+            table.Load(command.ExecuteReader());
+            foreach (DataRow line in table.Rows)
+            {
+                Idioma idioma = new Idioma()
+                {
+                    Id = Convert.ToInt32(line[0].ToString()),
+                    IdGuia = Convert.ToInt32(line[1].ToString()),
+                    Nome = line[2].ToString(),
+
+                };
+                idiomas.Add(idioma);
+            }
             return idiomas;
+        }
+
+        public int Cadastrar(Idioma idioma)
+        {
+            SqlCommand command = new Conexao().ObterConexao();
+
+            command.CommandText = @"INSERT INTO idiomas(nome, id_guia) OUTPUT INSERTED.ID VALUES (@NOME, @ID_GUIA)";
+            command.Parameters.AddWithValue("@NOME", idioma.Nome);
+            command.Parameters.AddWithValue("@ID_GUIA", idioma.IdGuia);
+
+            int id = Convert.ToInt32(command.ExecuteScalar().ToString());
+            return id;
+        }
+
+        public bool Alterar(Idioma idioma)
+        {
+            SqlCommand command = new Conexao().ObterConexao();
+
+            command.CommandText = @"UPDATE idiomas SET nome = @NOME, id_guia = @ID_GUIA WHERE id = @ID";
+
+            command.Parameters.AddWithValue("@NOME", idioma.Nome);
+            command.Parameters.AddWithValue("@ID_GUIA", idioma.IdGuia);
+            command.Parameters.AddWithValue("@ID", idioma.Id);
+            return command.ExecuteNonQuery() == 1;
+        }
+
+        public Idioma ObterPeloId(int id)
+        {
+            Idioma idioma = null;
+
+            SqlCommand command = new Conexao().ObterConexao();
+
+            command.CommandText = @"SELECT nome, id_guia FROM idiomas WHERE id = @ID";
+            command.Parameters.AddWithValue("@ID", id);
+
+            DataTable table = new DataTable();
+            table.Load(command.ExecuteReader());
+
+            if (table.Rows.Count == 1)
+            {
+                idioma = new Idioma();
+                idioma.Id = id;
+                idioma.Nome = table.Rows[0][0].ToString();
+                idioma.IdGuia = Convert.ToInt32(table.Rows[0][1].ToString());
+            }
+            return idioma;
         }
     }
 }
