c9d3464ed44bae8fdd7c4ca12d35598c3deac5c1
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 16:44:49 -0300

    Adicionado

--- TrabalhoFinal/Repository/IdiomaRepository.cs
+++ TrabalhoFinal/Repository/IdiomaRepository.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using Principal;
+using Principal.Database;
 
 namespace Principal.Repository
 {
     public class IdiomaRepository
     {
         public List<Idioma> ObterTodos()
+        {
+            List<Idioma> idiomas = new List<Idioma>();
+            SqlCommand command = new BancoDados().ObterConexao();
+            return idiomas;
+        }
     }
 }
