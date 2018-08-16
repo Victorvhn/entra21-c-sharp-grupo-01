a86edc2a8c623d72bf330f4c87c94ccbe2c8b5b8
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 14:57:29 -0300

    Class País

---
+++ TrabalhoFinal/Model/Pais.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+
+        public Continente Continente { get; set; }
+
+        public int IdContinente { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
