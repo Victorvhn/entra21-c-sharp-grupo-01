e3f00e08dd117fe0c86a97121c82893da0e110b0
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 15:09:09 -0300

    Class Viagem

---
+++ TrabalhoFinal/Model/Viagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Viagem
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public Pacote Pacote { get; set; }
+
+        public int IdPacote { get; set; }
+
+        public Guia Guia { get; set; }
+
+        public int IdGuia { get; set; }
+
+        public DateTime DataHorarioSaida { get; set; }
+
+        public DateTime DataHorarioVolta { get; set; }
+
+
+    }
+}
