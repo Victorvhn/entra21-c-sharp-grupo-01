f6a84ed5286f1dc712e5a1bc8a2f4270f49b9746
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 15:12:47 -0300

    HistoricoViagem

---
+++ TrabalhoFinal/Model/HistoricoViagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class HistoricoViagem
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public Pacote Pacote { get; set; }
+
+        public int IdPacote { get; set; }
+    }
+}
