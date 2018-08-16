4a40b1ba0f1e74061ee890bcb6656801e1f01f6f
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 16:39:46 -0300

    BancoDados e IdiomaRepository

---
+++ TrabalhoFinal/Repository/IdiomaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Principal.Repository
+{
+    public class IdiomaRepository
+    {
+        public List<Idioma> ObterTodos()
+    }
+}
