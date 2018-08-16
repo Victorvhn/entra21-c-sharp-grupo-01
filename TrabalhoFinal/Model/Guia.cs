60d22b5984edb79850e2276131d4618994688be6
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 16:07:03 -0300

    Alteração

--- TrabalhoFinal/Model/Guia.cs
+++ TrabalhoFinal/Model/Guia.cs
@@ -10,7 +10,7 @@ namespace Model
     {
         public string Login_ { get; set; }
 
-        public char Sexto { get; set; }
+        public char Sexo { get; set; }
 
         public int Id { get; set; }
 
