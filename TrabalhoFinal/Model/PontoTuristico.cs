db28776973b18de98111ae6d66398bb8e8b63d58
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 15:02:54 -0300

    Class PontoTuristico

---
+++ TrabalhoFinal/Model/PontoTuristico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PontoTuristico
+    {
+        public int Id { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public int IdEndereco { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
