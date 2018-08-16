e54362929c9a7572a3a7b52abb04dca4d1e4abfd
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 14:39:44 -0300

    Class Turista

---
+++ TrabalhoFinal/Model/Turista.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Turista
+    {
+        public Endereco Endereco { get; set; }
+
+        public string Senha { get; set; }
+
+        public char Ativo { get; set; }
+
+        public string Perfil { get; set; }
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Rg { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public int IdEndereco { get; set; }
+
+        public bool Premium { get; set; }
+    }
+}
