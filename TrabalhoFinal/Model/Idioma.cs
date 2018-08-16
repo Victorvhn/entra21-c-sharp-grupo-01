c4ecc1118d4ff1fb4a40837355d3d11e1f8b4e3f
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 14:53:02 -0300

    Class Idioma

---
+++ TrabalhoFinal/Model/Idioma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Idioma
+    {
+        public int Id { get; set; }
+
+        public Guia Guia { get; set; }
+
+        public int IdGuia{ get; set; }
+
+        public string Nome { get; set; }
+    }
+}
