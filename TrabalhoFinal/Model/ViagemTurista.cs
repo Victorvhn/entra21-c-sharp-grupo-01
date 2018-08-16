c64ef9ea1f6ab394d5db1e0d55e5288e187dee04
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-16 15:11:32 -0300

    Class ViagemTurista

---
+++ TrabalhoFinal/Model/ViagemTurista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ViagemTurista
+    {
+        public int Id { get; set; }
+
+        public Turista Turista { get; set; }
+
+        public int IdTurista { get; set; }
+
+        public Viagem Viagem{ get; set; }
+
+        public int IdViagem { get; set; }
+
+        public float Valor { get; set; }
+    }
+}
