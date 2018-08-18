26ad326bca7a69e716b07f575e613c5c477bca40
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-08-17 21:37:21 -0300

    adicionado controler guia mais precisa arrumar referencia do GuiaRepositorio

--- TrabalhoFinal/Principal/Controllers/PrincipalController.cs
+++ TrabalhoFinal/Principal/Controllers/PrincipalController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,5 +24,52 @@ namespace Principal.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult CadastroGuia()
+        {
+            ViewBag.TituloPagina = "Guia Cadastro";
+            ViewBag.Guia = new Guia();
+            return View();
+        }
+
+       /* Não ta funcionando Guia repositorio pq precisa da referencia e nao tem como adicionar rever em sala 
+        [HttpGet]
+        public ActionResult EditarGuia(int id)
+        {
+            Guia guia = new GuiaRepositorio().ObterPeloId(id);
+            ViewBag.Guia = guia;
+            ViewBag.TituloPagina = "Guia Editar";
+            return View();
+        } 
+
+        [HttpGet]
+        public ActionResult Excluir(int id)
+        {
+            bool apagado = new GuiaRepositorio().Excluir(id);
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Store(Guia guia)
+        {
+            if (ModelState.IsValid)
+            {
+                guia.Cpf = guia.Cpf.Replace(".", "").Replace("-", "");
+                int identificador = new GuiaRepositorio().Cadastrar(guia);
+                return RedirectToAction("Editar", new { id = identificador });
+            }
+
+            ViewBag.Guia = guia;
+            return View("Guia Cadastro");
+        }
+
+        [HttpPost]
+        public ActionResult Update(Guia guia)
+        {
+            bool alterado = new GuiaRepositorio().Alterar(guia);
+            return null;
+        }
+        */
     }
 }
