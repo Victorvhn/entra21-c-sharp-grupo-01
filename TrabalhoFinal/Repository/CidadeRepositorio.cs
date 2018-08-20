303c5203dbda55cc14cbd4788b2ccd2236162ee7
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-08-20 14:41:08 -0300

    mudei pra public class da cidade

--- TrabalhoFinal/Repository/CidadeRepositorio.cs
+++ TrabalhoFinal/Repository/CidadeRepositorio.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    class CidadeRepositorio
+    public class CidadeRepositorio
     {
         public List<Cidade> ObterTodos()
         {
