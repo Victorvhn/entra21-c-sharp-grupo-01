079cfa525f1508133ff789468124fc781367a7cb
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-08-21 15:10:57 -0300

    Ponto Turistico repositorio

--- TrabalhoFinal/Principal/Controllers/PontoTuristicoController.cs
+++ TrabalhoFinal/Principal/Controllers/PontoTuristicoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,43 @@ namespace Principal.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            ViewBag.TituloPagina = "Ponto Turistico Cadastro";
+            ViewBag.PontoTuristico = new PontoTuristico();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            PontoTuristico PontoTuristico = new PontosTuristosRepository().ObterPeloId(id);
+            ViewBag.PontoTuristico = PontoTuristico;
+            ViewBag.TituloPagina = "Ponto Turistico Editar";
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Excluir(int id)
+        {
+            bool apagado = new PontosTuristosRepository().Excluir(id);
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Store(PontoTuristico PontoTuristico)
+        {
+            int identificador = new PontosTuristosRepository().Cadastrar(PontoTuristico);
+            return null;
+        }
+        [HttpPost]
+        public ActionResult Update(PontoTuristico PontoTuristico)
+        {
+            bool alterado = new PontosTuristosRepository().Alterar(PontoTuristico);
+            return null;
         }
     }
 }
