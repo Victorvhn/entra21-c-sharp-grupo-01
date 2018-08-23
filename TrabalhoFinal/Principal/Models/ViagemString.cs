275e26c34af9bcf59de58b72ca5cb1ac0254cf70
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-08-23 14:08:10 -0300

    novos models string feitos

--- TrabalhoFinal/Principal/Models/ViagemString.cs
+++ TrabalhoFinal/Principal/Models/ViagemString.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,18 @@ namespace Principal.Models
 {
     public class ViagemString
     {
+        public string Id { get; set; }
+
+        public string Data { get; set; }
+
+        public string IdPacote { get; set; }
+
+        public string IdGuia { get; set; }
+
+        public string DataHorarioSaida { get; set; }
+
+        public string DataHorarioVolta { get; set; }
+
+        public List<Turista> Turistas { get; set; }
     }
 }
--- TrabalhoFinal/Principal/Models/ViagemTuristaString.cs
+++ TrabalhoFinal/Principal/Models/ViagemTuristaString.cs
@@ -7,5 +7,12 @@ namespace Principal.Models
 {
     public class ViagemTuristaString
     {
+        public string Id { get; set; }
+
+        public string IdTurista { get; set; }
+
+        public string IdViagem { get; set; }
+
+        public string Valor { get; set; }
     }
 }
