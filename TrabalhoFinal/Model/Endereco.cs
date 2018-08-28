667e844f609393ea7d99c9f37053cfe149fcd00a
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-28 14:38:45 -0300

    Alteração Model

--- TrabalhoFinal/Model/Endereco.cs
+++ TrabalhoFinal/Model/Endereco.cs
@@ -10,6 +10,10 @@ namespace Model
     {
         public int Id { get; set; }
 
+        public int IdCidade { get; set; }
+
+        public Cidade Cidade { get; set; }
+
         public string Cep { get; set; }
 
         public string Logradouro { get; set; }
