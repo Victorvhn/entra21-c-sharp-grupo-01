10581e6f7766ac0594b785635096b2fa89c9fd1e
LucasRodrigoK <lucasrkthd@gmail.com>
2018-08-30 14:44:56 -0300

    popular-select-cidade feito

---
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectCidade/popular-select-cidade.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#select-cadastro-cidade-idestado').select2({
+        ajax: {
+            url: '/Estado/ObterTodosPorSelect2',
+            dataType: 'json',
+
+        }
+    });
+});
