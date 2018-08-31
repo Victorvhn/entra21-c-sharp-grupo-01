483486555b3137952f4772535f5d72cd85b9e8f4
Marcos Mittag <Marquinhoszx_xd@outlook.com>
2018-08-31 14:41:52 -0300

    popular Select ponto turistico

--- TrabalhoFinal/Principal/Scripts/PopularSelects/SelectPontoTuristico/popular-select-PontoTuristico.js
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectPontoTuristico/popular-select-PontoTuristico.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function () {
+    $('#select-cadastro-ponto-turistico-endereco').select2({
+        ajax: {
+            url: '/Endereco/ObterTodosPorSelect2',
+            dataType: 'json',
+        }
+    });
+});
+       
