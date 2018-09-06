f460120c98c1be3df2e1be6868d77c7e502e3cdc
Victor Hugo do Nascimento <38699623+Viictorvhn@users.noreply.github.com>
2018-09-06 17:18:36 -0300

    Hv feito

--- TrabalhoFinal/Principal/Scripts/PopularSelects/SelectHistoricoViagem/popular-select-historicoViagem.js
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectHistoricoViagem/popular-select-historicoViagem.js
@@ -7,3 +7,12 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function () {
+    $('#select-editar-historico-viagem-idPacote').select2({
+        ajax: {
+            url: '/Pacote/ObterTodosPorJSONToSelect2',
+            dataType: 'json'
+        }
+    });
+});
