72a29b66cf633c117daf1c8e04b862c7e21adff9
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-09-10 17:20:27 -0300

    adicionado validação historico de viagem

--- TrabalhoFinal/Principal/Scripts/Validacao/validation-historico-viagem.js
+++ TrabalhoFinal/Principal/Scripts/Validacao/validation-historico-viagem.js
@@ -1,1 +1,19 @@
-﻿
+﻿$('#div-modal-cadastro-historico-viagem').validate({
+    rules: {
+        'historicoViagem.IdPacote': {
+            required: true
+        },
+        'historicoViagem.Data': {
+            required: true
+        },
+    },
+    messages: {
+        'historicoViagem.IdPacote': {
+            required: 'Este campo é obrigatório'
+        },
+        'historicoViagem.Data': {
+            required: 'Por favor insira uma data válida'
+        },
+    }
+
+});
