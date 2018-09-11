08a99fb30ffedc129d109359426f8432c01623ab
Lucas Rodrigo Krueger <lucasrkthd@gmail.com>
2018-09-11 16:25:24 -0300

    select2-cadastro e editar viagem

--- TrabalhoFinal/Principal/Scripts/Viagem/Cadastro/select2-cadastro-viagem.js
+++ TrabalhoFinal/Principal/Scripts/Viagem/Cadastro/select2-cadastro-viagem.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(document).ready(function () {
+    $('#select-modal-cadastro-viagem').select2({
+        ajax: {
+            url: '/Viagem/ObterTodosPorJSONToSelect2',
+            dataType: 'json'
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#select-modal-editar-viagem').select2({
+        ajax: {
+            url: '/Viagem/ObterTodosPorJSONToSelect2',
+            dataType: 'json'
+        }
+    });
+});
