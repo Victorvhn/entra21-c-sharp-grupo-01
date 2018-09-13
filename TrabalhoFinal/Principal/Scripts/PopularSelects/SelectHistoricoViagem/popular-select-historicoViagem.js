4a3a0c2a6383808881d6d068ca5c406081a78ab1
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-09-13 17:23:04 -0300

    ajustaod bvaldiaçao historico viagem

--- TrabalhoFinal/Principal/Scripts/PopularSelects/SelectHistoricoViagem/popular-select-historicoViagem.js
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectHistoricoViagem/popular-select-historicoViagem.js
@@ -1,18 +1,24 @@
 ﻿"use strict";
 $(document).ready(function () {
     $('#select-cadastro-historico-viagem-idPacote').select2({
+        placeholder: "Selecione um Pacote",
         ajax: {
             url: '/Pacote/ObterTodosPorJSONToSelect2',
             dataType: 'json'
         }
+    }).on('change', function (e) {
+        $('#form-modal-cadastro-historico-viagem').valid()
     });
 });
 
 $(document).ready(function () {
     $('#select-editar-historico-viagem-idPacote').select2({
+        placeholder: "Selecione um Pacote",
         ajax: {
             url: '/Pacote/ObterTodosPorJSONToSelect2',
             dataType: 'json'
         }
+    }).on('change', function (e) {
+        $('#form-modal-ediatar-historico-viagem').valid()
     });
 });
