1ccaae9a1f613b19861333fd1513f7dc2ff4d90b
LucasRKrueger <lucasrkthd@gmail.com>
2018-09-13 21:14:33 -0300

    popular-select-pacote-ponto-turistico, modalCadastro, modal Editar

---
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectPacotePontoTuristico/popular-select-pacote-ponto-turistico.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#select-cadastro-pacote-ponto-turistico-pacote').select2({
+        ajax: {
+            url: '/Pacote/ObterTodosPorJSONToSelect2',
+            dataType: 'json'
+        }).on('change', function () {
+            $('select-cadastro-pacote-ponto-turistico-ponto-turistico').select2('open');
+            });
+    });
+    $('#select-cadastro-pacote-ponto-turistico-ponto-turistico').select2({
+        ajax: {
+            url: '/PontoTuristico/ObterTodosPorJSONSelect2',
+            dataType: 'json'
+        }
+    }):
+});
