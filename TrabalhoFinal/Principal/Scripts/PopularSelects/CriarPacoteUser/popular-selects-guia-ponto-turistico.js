19eba5146b88a8319db51474f90da1a58c9865b2
Victor Hugo do Nascimento <38699623+Viictorvhn@users.noreply.github.com>
2018-09-14 14:39:42 -0300

    Js criado

---
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/CriarPacoteUser/popular-selects-guia-ponto-turistico.js
@@ -0,0 +1,9 @@
+﻿﻿$(document).ready(function () {
+    $('#select-destino-pacote-user').select2({
+        placeholder: "Selecione um Destino",
+        ajax: {
+            url: '/Estado/ObterTodosPorJSONToSelect2',
+            dataType: 'json'
+        }
+    });
+});
