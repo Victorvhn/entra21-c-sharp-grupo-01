d7d1894fb74f7622eaf86e138797fb96e374782b
Lucas Rodrigo Krueger <lucasrkthd@gmail.com>
2018-09-14 13:52:35 -0300

    Pacote Ponto Turistico Popular Select
    Agora aparece no modal as informações, ainda não cadastra

--- TrabalhoFinal/Principal/Scripts/PopularSelects/SelectPacotePontoTuristico/popular-select-pacote-ponto-turistico.js
+++ TrabalhoFinal/Principal/Scripts/PopularSelects/SelectPacotePontoTuristico/popular-select-pacote-ponto-turistico.js
@@ -3,14 +3,17 @@
         ajax: {
             url: '/Pacote/ObterTodosPorJSONToSelect2',
             dataType: 'json'
+        }
         }).on('change', function () {
             $('select-cadastro-pacote-ponto-turistico-ponto-turistico').select2('open');
-            });
-    });
+        });
+
     $('#select-cadastro-pacote-ponto-turistico-ponto-turistico').select2({
         ajax: {
             url: '/PontoTuristico/ObterTodosPorJSONSelect2',
             dataType: 'json'
         }
-    }):
+    });
 });
+
+
