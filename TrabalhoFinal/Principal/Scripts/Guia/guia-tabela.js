4b876c8d452cf7a8eb5aa6bd4883b264590abd2b
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-09-18 17:00:41 -0300

    validação cpf do guia feito, falta terminar os outros campos

--- TrabalhoFinal/Principal/Scripts/Guia/guia-tabela.js
+++ TrabalhoFinal/Principal/Scripts/Guia/guia-tabela.js
@@ -117,72 +117,114 @@
     });
 
     //Validação Modal Cadastro
-    $('#form-modal-cadastro-guia').validate({
-        errorClass: 'form-control-danger',
-        validClass: 'form-control-success',
-        highlight: function (element) {
-            jQuery(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function (element) {
-            jQuery(element).closest('.form-group').removeClass('has-error');
-        },
-        errorPlacement: function (error, element) {
-            $(element).parent().append(error[0])
-        },
-
-
-        rules: {
-            'guia.Nome': {
-                required: true,
-                rangelength: [3, 30]
+    function init() {
+        $('#form-modal-cadastro-guia').validate({
+            errorClass: 'form-control-danger',
+            validClass: 'form-control-success',
+            highlight: function (element) {
+                jQuery(element).closest('.form-group').addClass('has-error');
             },
-            'guia.Sobrenome': {
-
+            unhighlight: function (element) {
+                jQuery(element).closest('.form-group').removeClass('has-error');
             },
-            'guia.Rg': {
-
-            },
-            'guia.Cpf': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
-            },
-            '': {
-
+            errorPlacement: function (error, element) {
+                $(element).parent().append(error[0])
             },
 
+            rules: {
+                'guia.Nome': {
+                    required: true,
+                    rangelength: [3, 30]
+                },
+                'guia.Sobrenome': {
+                    required: true,
+                    rangelength: [3, 30]
+                },
+                'guia.Rg': {
+                    required: true,
+                    digits: true
+                },
+                'guia.Cpf': {
+                    required: true,
+                    verificaCPF: true
+                },
+                '': {
 
-        },
-        messages: {
-            'guia.Nome': {
-                required: 'Nome deve ser preenchido.',
-                rangelength: 'Nome deve conter de  {0} a {1} caracteres.'
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+                '': {
+
+                },
+
+
+            },
+            messages: {
+                'guia.Nome': {
+                    required: 'Nome deve ser preenchido.',
+                    rangelength: 'Nome deve conter de  {0} a {1} caracteres.'
+                },
+                'guia.Sobrenome': {
+                    required: 'Sobrenome deve ser preenchido.',
+                    rangelength: 'Sobrenome deve conter de  {0} a {1} caracteres.'
+                },
+                'guia.Rg': {
+                    required: 'RG deve ser preenchido.',
+                    digits: 'Rg deve conter somente digitos.'
+                },
+                'guia.Cpf': {
+                    required: 'CPF deve ser preenchido.'
+
+                },
             }
-        }
 
-    });
+        });
+    }
+
+    jQuery.validator.addMethod("verificaCPF", function (value, element) {
+        if (value.length < 11) return false;
+        value = value.replace('.', '');
+        value = value.replace('.', '');
+        cpf = value.replace('-', '');
+        
+        while (cpf.length < 11) cpf = "0" + cpf;
+        var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+        var a = [];
+        var b = new Number;
+        var c = 11;
+        for (i = 0; i < 11; i++) {
+            a[i] = cpf.charAt(i);
+            if (i < 9) b += (a[i] * --c);
+        }
+        if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11 - x }
+        b = 0;
+        c = 11;
+        for (y = 0; y < 10; y++) b += (a[y] * c--);
+        if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11 - x; }
+        if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) return false;
+        return true;
+    }, "Informe um CPF válido.");
+
+    $(document).ready(init);
 
     //Salvar modal cadastro
     $("#botao-salvar-modal-cadastrar-guia").on("click", function () {
