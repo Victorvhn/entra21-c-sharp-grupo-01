f8e6b5f535a76f28bda8317eee06765777bc3826
LucasRKrueger <lucasrkthd@gmail.com>
2018-09-25 15:48:32 -0300

    Alteração Resource

--- TrabalhoFinal/Principal/Resources/Resource1.Designer.cs
+++ TrabalhoFinal/Principal/Resources/Resource1.Designer.cs
@@ -19,7 +19,7 @@ namespace Principal.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -63,9 +63,9 @@ namespace Principal.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Ação.
         /// </summary>
-        public static string acao {
+        public static string Acao {
             get {
-                return ResourceManager.GetString("acao", resourceCulture);
+                return ResourceManager.GetString("Acao", resourceCulture);
             }
         }
         
