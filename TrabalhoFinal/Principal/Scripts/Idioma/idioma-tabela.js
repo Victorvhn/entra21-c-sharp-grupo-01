34560c79ac16608892858dd7c7c8fa05e70f42b0
LucasRKrueger <lucasrkthd@gmail.com>
2018-09-27 01:04:19 -0300

    js idioma alterado

--- TrabalhoFinal/Principal/Scripts/Idioma/idioma-tabela.js
+++ TrabalhoFinal/Principal/Scripts/Idioma/idioma-tabela.js
@@ -68,7 +68,7 @@
                     $('#idioma-modal-cadastro').modal('hide');
                     $(function () {
                         new PNotify({
-                            title: 'Sucesso!',
+                            title: STRINGS.sucesso,
                             text: nomeVar + " " + STRINGS.cadastrado,
                             type: 'success'
                         });
@@ -134,7 +134,7 @@
                         $('#idioma-modal-editar').modal('hide');
                         $(function () {
                             new PNotify({
-                                title: 'Sucesso!',
+                                title: STRINGS.sucesso,
                                 text: STRINGS.alterado,
                                 type: 'info'
                             });
@@ -165,8 +165,8 @@
                 var resultado = JSON.parse(data);
                 if (resultado == 1) {
                     new PNotify({
-                        title: 'Desativado!',
-                        text: nome + " " + STRINGS.desativado,
+                        title: STRINGS.desativado,
+                        text: nome + " " + STRINGS.desativadoSucesso,
                         type: 'success'
                     });
 
